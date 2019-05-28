ce98173fb6da6ff171609f3ba9c2cd045f49015f
idealik <shavl.mark@yandex.ru>
2019-05-28 17:52:34 +0300

    реализация паттерна builder

---
+++ src/BuilderPatternCar.java
@@ -0,0 +1,52 @@
+public class BuilderPatternCar {
+
+    //задаем параметры машины
+    private String name;
+    private String color;
+    private int maxSpeed;
+
+    private BuilderPatternCar(Builder builder) {
+        this.name = builder.name;
+        this.color = builder.color;
+        this.maxSpeed = builder.maxSpeed;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+
+    static class Builder {
+        //задаем параметры для строителя
+        private String name;
+        private String color;
+        private int maxSpeed;
+
+        public Builder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder setColor(String color) {
+            this.color = color;
+            return this;
+        }
+
+        public Builder setMaxSpeed(int maxSpeed) {
+            this.maxSpeed = maxSpeed;
+            return this;
+        }
+        public BuilderPatternCar build(){
+            return new BuilderPatternCar(this);
+        }
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,23 @@ import factoryPattern.MyFactoryPattern;
 public class Main {
 
     public static void main(String[] args) {
+        /*
+        Этот паттерн необходим для динамической замены одного продукта другим.
+        Т. е. код уже скомпилирован, но у тебя все же есть возможность выбора любого другого продукта.
+        Так же он позволят сократить время на добавление другого продукта.
+         */
+        System.out.println("Factory");
         MyFactoryPattern myFactoryPattern = new MyFactoryPattern("digital");
+
+        //Позволяет не создавать множество конструкторов, чтобы реализовать гибкий объект
+        System.out.println("Builder");
+        BuilderPatternCar builderPatternCar = new BuilderPatternCar.Builder()
+                .setName("BMW")
+                .setColor("Black")
+                .setMaxSpeed(500)
+                .build();
+        System.out.println(builderPatternCar.getName()
+                + " " + builderPatternCar.getColor()
+                + " " + builderPatternCar.getMaxSpeed());
     }
 }
